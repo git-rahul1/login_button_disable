f23b8803c0af4db85b464888177a7bfb65eb4b73
Md. Rahul Reza <git-rahul@devxhub.com>
2023-03-30 21:55:32 +0600

    validation not yet complete

---
+++ android/app/src/main/kotlin/com/devxhub/loginscreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devxhub.loginscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Presentation/Declarations/Images/images.dart
@@ -0,0 +1,5 @@
+List<String> loginImages = [
+  "images/Loginpage/lock.png",
+  "images/Loginpage/lock_1.gif",
+  "images/Loginpage/lock_2.gif",
+];
